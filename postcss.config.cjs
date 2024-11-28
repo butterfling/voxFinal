870d7d59ed9d2650940c59827ca287abcf8641ba
Saahil Rathore <500097152@stu.upes.ac.in>
2024-11-28 15:46:33 +0530

    updated configs and readme2

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,14 @@
+
+// https://nextjs.org/docs/advanced-features/using-postcss-plugins
+
+
 const config = {
+  
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+
 };
 
 module.exports = config;
